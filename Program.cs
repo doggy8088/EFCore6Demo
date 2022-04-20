23a5dd81f27bc3503b5cbec5241e3f16b0f5b6af
Will <doggy.huang@gmail.com>
2022-04-20 23:19:56 +0800

    Initialize the DbContext for ASP.NET Core

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ContosoUniversityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
