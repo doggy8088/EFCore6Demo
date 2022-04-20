e2979eabbbd9706907e20fcc08dd4e0906093f33
Will <doggy.huang@gmail.com>
2022-04-20 23:51:37 +0800

    新增 DepartmentsController
    https://localhost:7295/api/Departments

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,22 @@
+namespace EFCore6Demo.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DepartmentsController : ControllerBase
+{
+    private readonly ILogger<DepartmentsController> _logger;
+    private readonly ContosoUniversityContext _db;
+
+    public DepartmentsController(ILogger<DepartmentsController> logger, ContosoUniversityContext db)
+    {
+        _logger = logger;
+        _db = db;
+    }
+
+    [HttpGet("")]
+    public ActionResult<IEnumerable<Department>> GetDepartments()
+    {
+        return this._db.Departments.ToList();
+    }
+
+}
