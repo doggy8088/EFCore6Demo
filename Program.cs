881685e746b8a15f15aa790dbc846e02475204f1
Will <doggy.huang@gmail.com>
2022-04-21 23:21:44 +0800

    加入 JsonOptions 設定，讓 JSON 輸出排版過，方便閱讀

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ContosoUniversityContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.WriteIndented = true;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
